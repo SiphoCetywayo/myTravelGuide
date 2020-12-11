29b5779754e3a9767e06ab7ab5de2674f9f45bd7
cetywayos <sipho.cetywayo@gmail.com>
2020-12-11 10:24:37 +0200

    Intial Project commit

---
+++ app/src/main/java/com/example/mytravelguide/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.mytravelguide;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    TabItem tabHotels;
+    TabItem tabAttractions;
+    TabItem tabRestaurants;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getResources().getString(R.string.app_name));
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabHotels = findViewById(R.id.tabAccommodations);
+        tabAttractions = findViewById(R.id.tabAttractions);
+        tabRestaurants = findViewById(R.id.tabRestaurants);
+        viewPager = findViewById(R.id.viewPager);
+
+        PageAdapter pageAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pageAdapter);
+
+    }
+}
---
+++ app/src/main/java/com/example/mytravelguide/PageAdapter.java
@@ -0,0 +1,38 @@
+package com.example.mytravelguide;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+/*Note: Android FragmentPagerAdapter is used when you have a limited number of tabs that you want to swipe through.
+If you have a dynamic tabs then you need to use Android FragmentStatePagerAdapter.*/
+
+public class PageAdapter extends FragmentPagerAdapter {
+    private int numOfTabs;
+    public PageAdapter(@NonNull FragmentManager fm, int numOfTabs) {
+        super(fm, numOfTabs);
+        this.numOfTabs = numOfTabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new HotelsFragment();
+            case 1:
+                return new AttractionsFragment();
+            case 2:
+                return new RestaurantsFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+
+        return numOfTabs;
+    }
+}
